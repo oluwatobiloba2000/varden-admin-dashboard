9d9f29886e98fd684e7d6a1e66568f1af14f6cf8
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2021-04-12 14:27:21 +0100

    modified color mode switcher

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
+import { useColorMode, useColorModeValue, IconButton, Text } from '@chakra-ui/react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
 export const ColorModeSwitcher = props => {
@@ -8,16 +8,20 @@ export const ColorModeSwitcher = props => {
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
+    <span style={{display: 'flex',
+      alignItems: 'center'}} onClick={toggleColorMode}>
     <IconButton
-      size="md"
-      fontSize="lg"
+      size="sm"
+      fontSize="sm"
+      minWidth="14px"
+      marginRight="11px"
       aria-label={`Switch to ${text} mode`}
       variant="ghost"
-      color="current"
-      marginLeft="2"
-      onClick={toggleColorMode}
+      color="black"
       icon={<SwitchIcon />}
       {...props}
     />
+     <Text color="black" marginTop="-2px" fontSize="14px">switch to {text}</Text> 
+    </span>
   );
 };
